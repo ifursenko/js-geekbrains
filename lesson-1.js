2e40d1bd251448af62c4167831ca1972a3f72a50
Ivan Fursenko <ifursenko2001@gmail.com>
2021-03-27 20:00:48 +0400

    lesson-1

---
+++ lesson-1.js
@@ -0,0 +1,17 @@
+//1
+let celsiusToFahrenheit = +prompt('Введите температуру в цельсиях', 100)
+celsiusToFahrenheit = (9 / 5) * celsiusToFahrenheit + 32
+alert("Температура в форенгейтах = " + celsiusToFahrenheit)
+
+
+//2
+let name = "Василий"
+let admin
+admin = name
+alert(admin)
+
+//3
+let sum = 1000 + "108"
+alert("Сумма 1000 + '108' = " + sum)
+
+
